239b34d7003f45502963759d63e578ea07fdba38
Rafael Yapchiongco <jr.yapchiongco@gmail.com>
2022-05-10 02:50:01 +0800

    New Design and Functions

---
+++ js/winbox.maker.js
@@ -0,0 +1,45 @@
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    background: '#00aa00',
+    //modal: true,
+    width: '400px',
+    height: '500px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#ff2a6d')
+    },
+    onblur: function () {
+      this.setBackground('#005678')
+    },
+  })
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    background: '#00aa00',
+    width: '400px',
+    height: '400px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#ff2a6d')
+    },
+    onblur: function () {
+      this.setBackground('#005678')
+    },
+  })
+})
