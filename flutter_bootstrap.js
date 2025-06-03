46bdfa30143155e74b4eb7b8a9c3b49c9462f5bb
Rafael Yapchiongco <51372391+stupidyaps@users.noreply.github.com>
2025-06-03 21:34:56 +0800

    Updated the Resume Page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3503757386"
+    serviceWorkerVersion: "4240524543"
   }
 });
